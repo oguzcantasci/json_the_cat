7ba386b963023f26ef88df1f3b8f8a0911873712
Ozzy Tasci <oguzcantasci@gmail.com>
2022-12-26 10:42:13 -0500

    Handle the request errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,6 +3,12 @@ const request = require(`request`);
 const breed = process.argv[2]; // Get the breed name from commandline
 
 request(`https://api.thecatapi.com/v1/breeds/search?name=${breed}`, (error, response, body) => {
+  if (error) {
+    console.log("!!!!!!Error!!!!!!\n",
+      `Error Numner: ${error.errno}\n`,
+      `Error Code: ${error.code}`);
+  }
+
   const data = JSON.parse(body);
   console.log(data[0].description);
 });
